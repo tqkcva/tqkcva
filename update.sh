23dd5c0efa93388e7e00f48695a93f1bd352627a
Kieu Tra <tqkcva@gmail.com>
2024-03-10 21:38:06 +0700

    Update

--- update.sh
+++ update.sh
@@ -4,3 +4,10 @@ git add $IN_FILE
 git commit -m "Update $IN_FILE"
 git push origin info
 
+if [ $? -eq 0 ]; then
+  echo "Update succeeded"
+else
+  echo "Update failed with status $?"
+  pause 10
+  git push origin info
+fi
